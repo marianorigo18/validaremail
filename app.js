917feba0c445d446b359e93f1d652560e887a7f4
marianorigo18 <marianowagnerrigo@gmail.com>
2022-12-16 18:14:37 -0300

    agregando mas validaciones al formulario

--- app.js
+++ app.js
@@ -3,12 +3,29 @@ document.addEventListener('DOMContentLoaded', function(){
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
 
     inputEmail.addEventListener('blur', validar);
     inputAsunto.addEventListener('blur', validar);
     inputMensaje.addEventListener('blur', validar);
 
     function validar(e){
-        console.log(e.target.value)
+        if(e.target.value.trim() == ''){
+            enviarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement)
+        }else{
+            console.log(e.target.value);
+        }
+    }
+
+    function enviarAlerta(mensaje, referencia){
+
+        const alerta = referencia.querySelector('.alerta');
+        if(alerta){
+            alerta.remove();
+        }
+        const error = document.createElement('P');
+        error.textContent = mensaje;
+        error.classList.add('alerta');
+        referencia.appendChild(error)
     }
 })
