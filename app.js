b284072410c03fdefaa85da5d8dfb5bc2381f218
marianorigo18 <marianowagnerrigo@gmail.com>
2022-12-19 10:50:28 -0300

    se agrego una validacion para que se active el boton de submit si los campos ingresados son validos

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
 
+    const email = {
+        email: '',
+        asunto: '',
+        mensaje: ''
+    }
+
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
+    const btnSubmit = document.getElementById('btn-submit');
 
     inputEmail.addEventListener('blur', validar);
     inputAsunto.addEventListener('blur', validar);
@@ -19,8 +26,13 @@ document.addEventListener('DOMContentLoaded', function(){
             return;
         }
         limpiarAlerta(e.target.parentElement)
-    }
 
+        //asignar valores
+        email[e.target.name] = e.target.value.trim().toLowerCase()
+    
+        //comprobar el objeto de email
+        comprobarEmail();
+     }
     function enviarAlerta(mensaje, referencia){
         limpiarAlerta(referencia)
         const error = document.createElement('P');
@@ -35,8 +47,16 @@ document.addEventListener('DOMContentLoaded', function(){
         }
     }
     function validarEmail(email){
-        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ //busca un patron en una cadena de texto 
+        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/; //busca un patron en una cadena de texto 
         const resultado = regex.test(email);
         return resultado;
+    }
+    function comprobarEmail(){
+        if(Object.values(email).includes('')){;//si almenos uno de estos valores tiene un string vacio = true, con esta validacion podemos saber si todos lo campos estan completos para despues validar el botn de envio.
+            console.log('aun hay campos vacios')
+        }else{
+            console.log('todos los campo estan comppletados')
+            btnSubmit.disabled = false;
+        }
     }
 })
